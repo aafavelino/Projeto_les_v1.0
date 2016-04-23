689fa6481302fc1ffbedc04e53c9962cf2792ed0
Adelino Afonso <adelino-afonso@hotmail.com>
2016-04-23 10:20:36 -0300

    Update drive_les_v1.cpp

--- drive_les_v1.cpp
+++ drive_les_v1.cpp
@@ -6,19 +6,43 @@ using std::cout;
 #include "les_v1.h"
 
 int main ( void ) {
-    SNPtr pHead = NULL;// nullptr;
-    for (int i = 0; i < 10; ++i)
-    {
-    	pushFront( pHead, i );
-    }
-    for (int i = 0; i < 10; ++i)
-    {
-    	//pushBack( pHead, i );
-    }
-    	int j;
-    	popFront(pHead, j);
-    	popBack(pHead, j);
+    SNPtr pHead = NULL;
+	int aux;
 
+	std::cout << ">>> Teste length..." << std::endl; 
+    std::cout << length(pHead) << std::endl; 
+    std::cout << ">>> Teste clear..." << std::endl;
+    	clear(pHead);   
+
+    std::cout << ">>> Teste pushFront..." << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+    	pushFront( pHead, i ) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
+    }
+
+    std::cout << ">>> Teste pushBack..." << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+    	pushBack( pHead, i ) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
+    }
+
+    std::cout << ">>> Teste empty..." << std::endl;    
+       	empty( pHead) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
+
+    std::cout << ">>> Teste popFront..." << std::endl;
+    	popFront(pHead, aux);
+    std::cout << ">>> Teste popBack..." << std::endl;    	
+    	popBack(pHead, aux);
+
+    std::cout << ">>> Teste front..." << std::endl;    
+       	front(pHead, aux) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
+    std::cout << ">>> Teste back..." << std::endl;    
+       	back(pHead, aux) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
+   
+    std::cout << ">>> Teste insert..." << std::endl;        	
+    	insert(pHead, pHead->mpNext, aux) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;    
+    std::cout << ">>> Teste remove..." << std::endl;        	
+    	remove(pHead, pHead->mpNext, aux) == true ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
 
 
 
